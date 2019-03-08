6bb8782ec900ffec1f6cbd2997def1d434171a7a
padarshr <padarshr@codeaurora.org>
2019-03-08 19:38:03 +0530

    common: Define the partition source order for product/build properties pickup
    Define ro.product.property_source_order to
    "odm,vendor,product,system_ext,system".
    Build/product properties like
    ro.build.[description|display.id|product],
    ro.product.[brand|device|manufacturer|model|name] and
    ro.build.fingerprint would be picked up from the partitions
    in the above defined order.
    
    Change-Id: If5e7a1e627723623d04b88adda6bb60bb1e6dd84

--- common.mk
+++ common.mk
@@ -185,6 +185,10 @@ include $(QCOM_COMMON_PATH)/components.mk
 # Filesystem
 TARGET_FS_CONFIG_GEN += $(QCOM_COMMON_PATH)/config.fs
 
+# Partition source order for Product/Build properties pickup.
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.product.property_source_order=odm,vendor,product,system_ext,system
+
 # Power
 ifneq ($(TARGET_PROVIDES_POWERHAL),true)
 ifneq ($(TARGET_PROVIDES_XIAOMI_POWERHAL), true)

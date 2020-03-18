198576e5d7ef577503dd265e79e3b5d51ad415fd
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:35:44 +0530

    common: Add common QTI components
     - To use this, set TARGET_COMMON_QTI_COMPONENTS := <components>.
       e.g. TARGET_COMMON_QTI_COMPONENTS := av bt perf
     - Configurations go inside qti-components/<component>/qti-<component>.mk
     - Blob list go in qti-components/<component>/proprietary-files.txt
    
    Change-Id: I9868b02da59c69326f3587cf338187753955c1db
    
    this is a kang from qcom/common from PA but I move to a different repo cuz i using stock CAF device/qcom/common
    
    this is usefull :)
    
    Signed-off-by: klozz <klozz@vivaldi.net>

---
+++ common.mk
@@ -0,0 +1,38 @@
+#
+# Copyright 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xperience/common
+
+# QTI common components
+ifneq (,$(filter av, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/av/qti-av.mk
+endif
+
+ifneq (,$(filter bt, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/bt/qti-bt.mk
+endif
+
+ifneq (,$(filter perf, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/perf/qti-perf.mk
+endif
+
+ifneq (,$(filter telephony, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/telephony/qti-telephony.mk
+endif
+
+ifneq (,$(filter wfd, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/wfd/qti-wfd.mk
+endif

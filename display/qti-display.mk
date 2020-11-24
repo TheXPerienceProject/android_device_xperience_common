AOSPA/android_device_qcom_common

common: only inherit display HAL makefiles if we build vendor
Change-Id: I3e732809feb2f1a34830095fdce97c55cfc63d74

display/qti-display.mk
@@ -18,8 +18,10 @@ $(error "TARGET_BOARD_PLATFORM is not defined yet. Please define in your device 
 endif
 
 # Include display HAL makefiles.
+ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM),)
 include hardware/qcom/display/config/display-board.mk
 include hardware/qcom/display/config/display-product.mk
+endif
 include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
 include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
 

TheXPerienceProject/android_device_xperience_common

display/qti-display.mk
@@ -18,8 +18,10 @@ $(error "TARGET_BOARD_PLATFORM is not defined yet. Please define in your device 
 endif
 
 # Include display HAL makefiles.
+ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
 include hardware/qcom/display/config/display-board.mk
 include hardware/qcom/display/config/display-product.mk
+endif
 include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
 include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
 
ca132523be85bbf0302536592a9d77c0ff6d87b4
Alexander Koskovich <zvnexus@outlook.com>
2021-02-15 13:34:20 -0700

    common: Mov performance RC to proper component.
    Change-Id: I5c19db40049f500f1ef4ef9639f0c2d22f6233e6
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- perf/qti-perf.mk
+++ perf/qti-perf.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Paranoid Android
+# Copyright (C) 2021 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/xperience/common/perf
+
+# Boot Jars
 PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance

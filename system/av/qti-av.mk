f5e0a7e46bd3fb6aa3e7a5b6385b90190963fa84
Juhyung Park <qkrwngud825@gmail.com>
2022-02-19 23:33:29 +0900

    common: move vendor.mm.enable.qcom_parser to /system/build.prop
    This was never supposed to be in /vendor/build.prop.
    
    [    2.006654] init: Do not have permissions to set 'vendor.mm.enable.qcom_parser' to '16777215' in property file '/vendor/build.prop': SELinux permission check failed
    
    Change-Id: I9ee069e877994e54f1a5ff6e36a95c45b905990c
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- system/av/qti-av.mk
+++ system/av/qti-av.mk
@@ -19,5 +19,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libmediaplayerservice
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    vendor.mm.enable.qcom_parser=16777215
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/system/av/av-vendor.mk)

741f4d568aafe627c8edfe5e82d1f2fd863c36e1
klozz <carlosj@klozz.dev>
2022-06-04 03:35:07 +0200

    display: return removed configurations
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: I19000f3c3b29606e0813a3909997ae0eaba582e0

--- system/display/qti-display.mk
+++ system/display/qti-display.mk
@@ -13,12 +13,15 @@
 # limitations under the License.
 
 # Include display HAL makefiles.
-#ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
+-include hardware/qcom-caf/$(VARIANT)/display/config/display-board.mk
+-include hardware/qcom-caf/$(VARIANT)/display/config/display-product.mk
+
+# Include display HAL makefiles.
+ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
 include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
 include vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk
-#endif
-$(warning "vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk")
+endif
 
 # Permissions
 PRODUCT_COPY_FILES += \

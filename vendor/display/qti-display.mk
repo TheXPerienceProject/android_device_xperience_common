a1a96dfcdc419f66ac96532ef381f1973c6ef5f1
Jake Weinstein <jake@aospa.co>
2022-08-21 02:20:19 +0900

    vendor: display: Build AIDL Lights HAL for 5.10
    Change-Id: Ica7aeabb15b67e7440a5542115c5aa4ff7065567

--- vendor/display/qti-display.mk
+++ vendor/display/qti-display.mk
@@ -17,6 +17,21 @@
 -include hardware/qcom-caf/$(VARIANT)/display/config/display-product.mk
 $(warning "variant is $(VARIANT)")
 
+# Enable Legacy Lights HAL for <5.10 targets
+ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
+
+# Lights HAL
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti \
+    lights.qcom
+
+else # >= 5.10
+
+# Include QTI AIDL Lights HAL for 5.10
+-include vendor/qcom/opensource/lights/lights-vendor-product.mk
+
+endif # >= 5.10
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
@@ -24,10 +39,8 @@ PRODUCT_COPY_FILES += \
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk_platform.vendor \
-    android.hardware.lights-service.qti \
     libqdutils \
-    libqservice \
-    lights.qcom
+    libqservice
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/display/display-vendor.mk)

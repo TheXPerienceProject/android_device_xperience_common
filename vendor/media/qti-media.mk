AOSPA/android_device_qcom_common

vendor: media: Move bengal Codec2 prop definition to ODM
[1] - is set by default in media hal so we will create an override for that
[2] - https://git.codelinaro.org/clo/la/platform/hardware/qcom/media/-/blob/LA.UM.9.15.r1-06800-KAMORTA.QSSI13.0/product.mk#L31

Change-Id: Idea892c7b795461664afb0d5f04bb1d55494e8be
Signed-off-by: Wahid7852 <wahidzk0091@gmail.com>

vendor/media/qti-media.mk
@@ -69,7 +69,7 @@ endif
 #           QSSI is forcing this to '1'. Must be reverted
 ifeq ($(call is-board-platform-in-list, bengal), true)
     $(warning "Default Codec2.0 Enabled")
-    PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
+    PRODUCT_ODM_OVERRIDES += debug.stagefright.ccodec=4
 endif
 
 # Get non-open-source specific aspects.

TheXPerienceProject/android_device_xperience_common

vendor/media/qti-media.mk
@@ -70,7 +70,7 @@ endif
 #           QSSI is forcing this to '1'. Must be reverted
 ifeq ($(call is-board-platform-in-list, bengal), true)
     $(warning "Default Codec2.0 Enabled")
-    PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
+    PRODUCT_ODM_PROPERTIES += debug.stagefright.ccodec=4
 endif
 
 # Get non-open-source specific aspects.
47b13401290e0af1ee34f4ffcc47c135b00332b5
klozz <carlosj@klozz.dev>
2023-02-22 00:15:24 -0600

    vendor:telephony:enable fetching of QoS parameters on lahaina
    https://git.codelinaro.org/clo/la/platform/vendor/qcom/lahaina/-/commit/158861728ec6401d60f77972db4bd4d50ee84c73
    
    Change-Id: Ia086e853e4488bf7160b504c1147019ad12bdcf6
    Signed-off-by: klozz <carlosj@klozz.dev>

--- vendor/telephony/qti-telephony.mk
+++ vendor/telephony/qti-telephony.mk
@@ -41,7 +41,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1
 
-ifeq ($(TARGET_BOARD_PLATFORM), holi)
+ifeq ($(TARGET_BOARD_PLATFORM), holi lahaina)
 # Vendor property to enable fetching of QoS parameters via IQtiRadio HAL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.fetchqos=true

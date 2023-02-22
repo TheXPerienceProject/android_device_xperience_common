e0b8a0627c7bb5fcad728efad5cdc9d2709e62df
klozz <carlosj@klozz.dev>
2023-02-21 23:50:00 -0600

    bt: build libldacBT_bco
    Change-Id: I4e23c9b9321ad89f0e0af9a22509636431c298be
    Signed-off-by: klozz <carlosj@klozz.dev>

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -27,6 +27,7 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libldacBT_enc \
     libldacBT_abr \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor

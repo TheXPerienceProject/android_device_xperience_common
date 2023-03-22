AOSPA/android_device_qcom_common

vendor: media: Move media init scripts here
Change-Id: Ib74bcb2845e2fc342127a56e6b383da439f0e73e

vendor/media-5.4/qti-media-5.4.mk
@@ -40,5 +40,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
     ro.media.recorder-max-base-layer-fps=60
 
+# Media Init
+PRODUCT_COPY_FILES += \
+    device/qcom/common/vendor/media-5.4/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/media-5.4/media-5.4-vendor.mk)

TheXPerienceProject/android_device_xperience_common

Signed-off-by: klozz <carlosj@klozz.dev>

vendor/media-5.4/qti-media-5.4.mk
@@ -40,5 +40,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
     ro.media.recorder-max-base-layer-fps=60
 
+# Media Init
+PRODUCT_COPY_FILES += \
+    device/xperience/common/vendor/media-5.4/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/media-5.4/media-5.4-vendor.mk)
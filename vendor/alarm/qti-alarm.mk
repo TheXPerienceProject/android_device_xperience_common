00b2a46afb09da89d6013ce7720865cba74c2c96
Richard Raya <rdxzv.dev@gmail.com>
2023-09-12 19:46:23 -0300

    common: alarm: Add manifest entry
    If already present, remove the manifest entry from the device side; otherwise, this would trigger duplication build errors.
    
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Change-Id: I23df4eb99bbe97a1331cbf0e949a48353d8a8ca8
    Signed-off-by: klozz <carlosj@klozz.dev>

--- vendor/alarm/qti-alarm.mk
+++ vendor/alarm/qti-alarm.mk
@@ -4,5 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/xperience/common/vendor/alarm
+
+# Init script
+PRODUCT_PACKAGES += \
+    init.qcom.alarm.rc
+
+# Manifest
+DEVICE_MANIFEST_FILE += \
+    $(QCOM_COMMON_PATH)/vendor/alarm/alarm-manifest.xml
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/alarm/alarm-vendor.mk)

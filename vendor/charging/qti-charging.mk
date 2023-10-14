AOSPA/android_device_qcom_common

vendor: charging: Add soong namespaces
Change-Id: Ia12f777feef24a67a3e49c63483fdfb44e46ea31

vendor/charging/qti-charging.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/charging
+
 # Health
 ifeq ($(TARGET_USE_AIDL_QTI_HEALTH),true)
 $(call inherit-product, vendor/qcom/opensource/healthd-ext/health-vendor-product.mk)

TheXPerienceProject/android_device_xperience_common

Signed-off-by: klozz <carlosj@klozz.dev>

vendor/charging/qti-charging.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/xperience/common/vendor/charging
+
 # Health
 ifeq ($(TARGET_USE_AIDL_QTI_HEALTH),true)
 $(call inherit-product, vendor/qcom/opensource/healthd-ext/health-vendor-product.mk)
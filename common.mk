3f8b8327775d8145740411b2be4676aa91881e27
Michael Bestas <mkbestas@lineageos.org>
2024-09-22 16:01:16 +0300

    common: Add soong config for QTI thermal HAL
    <=sm8350:
    $(call soong_config_set,qti_thermal,netlink,false)
    
    >=sm8450:
    $(call soong_config_set,qti_thermal,netlink,true)
    
    Change-Id: I362efc809e7afbae38185431248ad70850904840
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -169,6 +169,10 @@ TARGET_KERNEL_VERSION ?= 3.18
 endif
 $(warning "$(TARGET_KERNEL_VERSION)")
 
+ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
+$(call soong_config_set,qti_thermal,netlink,false)
+endif
+
 ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 # Compatibility matrix

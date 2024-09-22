1d0c0a7134def7c61c8239de343afd3393865be4
Michael Bestas <mkbestas@lineageos.org>
2024-09-23 01:52:25 +0900

    vendor: media: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/media-legacy/qti-media-legacy.mk
+++ vendor/media-legacy/qti-media-legacy.mk
@@ -29,12 +29,6 @@ endif
 # Inherit configuration from the HAL.
 $(call inherit-product-if-exists, hardware/qcom-caf/$(VARIANT)/media/product.mk)
 
-# Media Codecs
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 # Media Profiles
 PRODUCT_COPY_FILES += \
     device/xperience/common/vendor/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml

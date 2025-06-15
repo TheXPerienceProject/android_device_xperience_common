68ba92cab8242c0300ca80e78a8d4751e348fa34
klozz <carlosj@klozz.dev>
2025-06-15 02:22:54 -0600

    Revert "bt:dont call qcom makefiles"
    This reverts commit 75c4e369d0c4679638d5a2dffc691e62b61ba788.

--- system/bt/qti-bt.mk
+++ system/bt/qti-bt.mk
@@ -14,16 +14,16 @@
 
 # Include QTI Bluetooth makefiles.
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
-#$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk)
-#$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
+$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk)
+$(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
 endif
 
 # Properties
-#ifneq ($(TARGET_USE_QTI_BT_STACK),false)
-#PRODUCT_SYSTEM_EXT_PROPERTIES += \
-#    persist.vendor.bt.a2dp.aac_whitelist=false \
-#    ro.bluetooth.library_name=libbluetooth_qti.so
-#endif
+ifneq ($(TARGET_USE_QTI_BT_STACK),false)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    ro.bluetooth.library_name=libbluetooth_qti.so
+endif
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/system/bt/bt-vendor.mk)
